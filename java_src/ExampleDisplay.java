a574a5856a45cca6e0028c67ddb91f20a5278787
JeanHany <jean_hany@hotmail.com>
2014-11-05 22:15:37 +0100

    Restart game and refactoring

--- java_src/ExampleDisplay.java
+++ java_src/ExampleDisplay.java
@@ -101,10 +101,11 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 	static BufferedWriter bufOut = null;
 	static Rectangle rect1 = null ;
 	static Rectangle rect2 = null ;
+	static ExampleDisplay window = null;
 
 	/* gameMap contains the plan of the sweets to collect initialized to
 	 * null by default */
-	static Circle[][] gameMap = new Circle[gridSize][gridSize]; 
+	static Circle[][] gameMap = null; 
 
 	public ExampleDisplay(){
 		super();
@@ -114,23 +115,7 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 		myContainer = getContentPane();
 		myContainer.setPreferredSize(new Dimension(cellSize * (gridSize + 1), cellSize * (gridSize + 1) ));
 		pack();
-
-		// adding the red circles for a bit of landscape
-//		Random rand = new Random();
-//
-//		for(int i = 0; i < numberOfSweets; i++) {
-//			int j, k;
-//			do {
-//				j = rand.nextInt(gridSize);
-//				k = rand.nextInt(gridSize);
-//			} while (gameMap[j][k]!=null);
-//
-//			gameMap[j][k] = new Circle(this);
-//			gameMap[j][k].setGridPos(j,k);
-//		} // EndFor i
-
 		setVisible(true);
-
 		moveTable.put(KeyEvent.VK_DOWN ,new int[] { 0,+1});
 		moveTable.put(KeyEvent.VK_UP   ,new int[] { 0,-1});
 		moveTable.put(KeyEvent.VK_LEFT ,new int[] {-1, 0});
@@ -146,39 +131,23 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 	/* where the real work happens: reacting to key being pressed */
 	public void keyPressed (KeyEvent ke){ 
 		int keyCode = ke.getKeyCode();
-		System.out.println("KeyCode "+keyCode);
 		if (!moveTable.containsKey(keyCode)) return ;
 		int[] test = moveTable.get(keyCode);
 		int[] newtest = {0, 0};
 		Point newpoint = rect1.getLocation();
 		newtest[0] = test[0] + newpoint.x;
 		newtest[1] = test[1] + newpoint.y;
-		System.out.println("newpoint"+newtest[0] +" "+newtest[1]);
+//		System.out.println("newpoint"+newtest[0] +" "+newtest[1]);
 		String send = newtest[0]+","+newtest[1];
-//		String recv = "";
 		try{
 			send_socket(send);
 //			System.out.println("Recv "+recv);
 		}catch(IOException io){
 			System.out.println("Exeption "+io);;
 		}
-//		System.out.println("Recv "+recv);
-//		if(recv == "ok")
-//			System.out.println("passe");
-//			myRectangle.moveRect(test);
-//		System.out.println("Map "+test[1]+ "map"+test[0]);
-//		if (gameMap[rect1.x][rect1.y]!=null) {
-//			pack();
-//			numberOfSweets--;
-//			if (numberOfSweets==0) {
-//				System.out.println("You've won. Congratulations!");
-//				System.exit(0);
-//			}
-//			System.out.println("Only "+numberOfSweets+" sweet(s) remaining...");
-//		}
 		repaint();
 	} // EndMethod keyPressed
-
+	// Changer localhost
 	public static void main(String[] a) throws IOException {
 		 socket= new Socket();
 		InetSocketAddress inetSocketAddress = new InetSocketAddress(InetAddress.getByName("localhost"), 5678);
@@ -191,18 +160,10 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 		in =	
 				new BufferedReader(
 						new InputStreamReader(socket.getInputStream()));
-		ExampleDisplay window = new ExampleDisplay();
-		// Fonction receive et fonction put triangle et fonction send4
-		//    Receive = receive();    		
-		String test = receive_ok();
-		test = receive_ok();
-		System.out.println("Add rectangle "+test);		    
-		rect1 = add_rectangle(test, window);
-		test = receive_ok();
-//		System.out.println("Add player 2 "+test);
-		rect2 = add_rectangle(test, window);
-		test = receive_ok();
-		add_cercle(test, window);
+		
+		// Fonction receive et fonction put triangle et fonction send4  		
+		window = new ExampleDisplay();
+		gameMap = new Circle[gridSize][gridSize];
 		new Thread() {
 		      @Override
 		      public void run() {
@@ -210,29 +171,21 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 		        while(true) { // This thread runs forever
 		        	try{
 		        	position = receive();
-//		    		System.out.println("Receive thread "+position);
 		        	}catch(IOException io){
 		    			position="ko";
 		    		}
 		      }
 		    }
 		}.start();
-//		if(test == "2")
-//	    {
-//	    	test = receive();
-//	    	System.out.println("passe "+test);
-//	    }
-		//    }
+		
 		System.out.println("Fini ");
 	} // EndMethod main
 
 	public static Rectangle add_rectangle(String place, ExampleDisplay window)
 	{
-
+		System.out.println("Add Rect "+ place);
 		String[] coord = place.split(",");
-//		System.out.println("Coord "+coord[1]);
 		int[] test = {Integer.parseInt(coord[0]), Integer.parseInt(coord[1])};
-//		System.out.println("Place "+place);
 		Rectangle myRectangle = new Rectangle(window) ;
 		myRectangle.moveRect(test);
 		myRectangle.repaint();
@@ -241,41 +194,56 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 
 	public static String receive() throws IOException
 	{
-//		String newSt ="";
 		String userInput = in.readLine();
-		//	  String newString = userInput.substring(userInput.length());
-//		System.out.println("Receive "+ userInput);
-		String[] tab_S = userInput.split(";");
-		System.out.println("String receive"+tab_S[0]+" "+tab_S[1]);
-		if(tab_S[0].equals("1") )
+		if(userInput.contains(";"))
 		{
-			System.out.println("triangle 1");
-			move_rectangle1(tab_S[1]);
-		}else if(tab_S[0].equals("2"))
-		{
-			System.out.println("triangle 2");
-			move_rectangle2(tab_S[1]);
-		}else if(tab_S[0].equals("3"))
-		{
-			remove_circle(tab_S[1]);
-		}else
-		{
-			System.exit(0);;
+			String[] tab_S = userInput.split(";");
+			if(tab_S[0].equals("1") )
+			{
+				move_rectangle1(tab_S[1]);
+			}else if(tab_S[0].equals("2"))
+			{
+				move_rectangle2(tab_S[1]);
+			}else if(tab_S[0].equals("3"))
+			{
+				remove_circle(tab_S[1], tab_S[2], tab_S[3]);
+			}else if(tab_S[0].equals("5"))
+			{
+				rect1 = add_rectangle(tab_S[1], window);
+			}else if(tab_S[0].equals("6"))
+			{
+				rect2 = add_rectangle(tab_S[1], window);
+			}else if(tab_S[0].equals("4"))
+			{
+				if(tab_S[1].equals("other"))
+				{
+					System.out.println("You loose");
+				}else if(tab_S[1].equals("equals"))
+				{
+					System.out.println("Equality");
+				}else{
+					System.out.println("You win");
+				}
+				send_socket("newgame");
+				window.dispose();
+				window = new ExampleDisplay();
+				gameMap = new Circle[gridSize][gridSize];
+	//			System.exit(0);
+			}
+			else if(tab_S[0].equals("7"))
+			{
+				String newstring = userInput.substring(2);
+//				System.out.println("Substring "+newstring);
+				add_cercle(newstring, window);
+			}
 		}
-//			  while ((userInput = in.readLine()) != "ok\n") {
-//			        System.out.println("echo: " + in.readLine());
-//			    }
 		return userInput;
 	}
 	
 	public static String receive_ok() throws IOException
 	{
 		String userInput = in.readLine();
-		//	  String newString = userInput.substring(userInput.length());
-		System.out.println("Receive "+ userInput);
-//			  while ((userInput = in.readLine()) != "ok\n") {
-//			        System.out.println("echo: " + in.readLine());
-//			    }
+		System.out.println("Receive ok"+userInput);
 		return userInput;
 	}
 
@@ -284,8 +252,6 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 	{
 		bufOut.write( send );
 		bufOut.flush();
-//		String userInput = receive_ok();
-//		return userInput;
 	}
 	
 	public static void move_rectangle1(String pos)
@@ -316,16 +282,28 @@ public class ExampleDisplay extends JFrame implements KeyListener {
 			gameMap[j][k] = new Circle(window);
 			gameMap[j][k].setGridPos(j,k);
 		}
-//		setVisible(true);
 	}
 	
-	public static void remove_circle(String pos)
+	public static void remove_circle(String pos, String score, String player)
 	{
 		String[] npos = pos.split(",");
 		int[] newpos = {Integer.parseInt(npos[0]), Integer.parseInt(npos[1])};
 		Circle c = gameMap[newpos[0]][newpos[1]];
 		myContainer.remove(c);
-//		pack();
+		System.out.println("Score player "+ player +": "+ score);
 		gameMap[newpos[0]][newpos[1]]=null;
 	}
+//	public static void start_game() throws IOException {
+//		window = new ExampleDisplay();
+//		String test = receive_ok();
+//		test = receive_ok();	    
+//		rect1 = add_rectangle(test, window);
+//		test = receive_ok();
+//		rect2 = add_rectangle(test, window);
+//		test = receive_ok();
+//		gameMap = new Circle[gridSize][gridSize];
+//		add_cercle(test, window);
+//		thread = new_thread();
+//		thread.start();
+//	}
 } // EndClass ExampleDisplay
--- src/serverErlang.erl
+++ src/serverErlang.erl
@@ -7,7 +7,7 @@
 -export([start_link/0, server/1, loop/1]).
 %% -export([add_player/1, send_position/1, test_position/2, get_state/0]).
 
--record(state, {nbplayer = 0, player1, player2, socket1, socket2, points, nbpas=0}).
+-record(state, {nbplayer = 0, player1, player2, socket1, socket2, points, nbpas=0, nbsweet1=0, nbsweet2=0}).
 -type state() :: #state{}.
 -record(point, {coordX, coordY}).
 -record(playercoord, {nbplayer, coord, nbsweet}).
@@ -22,9 +22,9 @@ init([]) ->
 	io:format("Init", []),
 	start_s(),
 	create_table(),
-%% 	put_coordonnees(10), Voir ou le mettre
+	%% 	put_coordonnees(10), Voir ou le mettre
 	State = #state{},
-%% 	{reply, _Reply, State} = get_state(),
+	%% 	{reply, _Reply, State} = get_state(),
 	{ok, State}.
 
 add_player(S) -> gen_server:call(?MODULE, {add_player, [S]}).
@@ -37,110 +37,48 @@ set_point(Point) -> gen_server:call(?MODULE, {set_point, [Point]}).
 
 %% get_state() -> gen_server:call(?MODULE, {get_state}).
 
-handle_call({test_position, [S, Pos]}, _From, #state{player1=P1, player2=P2, socket1=Socket1, socket2=Socket2, points=Points}=State) ->
-    
+handle_call({test_position, [S, Pos]}, _From, #state{player1=P1, player2=P2, socket1=Socket1, socket2=Socket2, points=Points,
+													 nbsweet1=Nb1, nbsweet2=Nb2, nbpas=Nbpas, nbplayer=Nbplay}=State) ->
+	
 	[X, Y] = binary:split(Pos, <<",">>),
 	X1 = list_to_integer(binary_to_list(X)),
 	Y1 = list_to_integer(binary_to_list(Y)),
 	case S of
 		Socket1 ->
-			[X11, Y11] = binary:split(P1, <<",">>),
-			X12 = list_to_integer(binary_to_list(X11)),
-			Y12 = list_to_integer(binary_to_list(Y11)),
-			X2 = mod(X12+X1, 20),
-			Y2 = mod(Y12+Y1, 20),
-			%% 			New_pos = {element(1, P1)+X1, element(2, P1)+Y1},
-			S1 = list_to_binary(integer_to_list(X2)),
-			S2 = list_to_binary(integer_to_list(Y2)),
-			New_pos = <<S1/binary, <<",">>/binary, S2/binary>>,
-			%% 			io:format("New pos 1 ~p ~n", [P2]),
-			%% 			io:format("New pos 1 ~p ~n", [New_pos]),
+			New_pos = calcul_newPos(P1, X1, Y1),
 			if
 				P2 == New_pos -> 
 					Reply = ko,
 					State2 = State;
-				true -> 
-					
+				true -> 					
 					Reply = ok,
 					%% Calculer new pos														
-					
-					Posnew = <<<<"1;">>/binary, Pos/binary, <<"\n">>/binary>>,
-					Posnew2 = <<<<"2;">>/binary, Pos/binary, <<"\n">>/binary>>,								
-					gen_tcp:send(Socket1, Posnew),
-					gen_tcp:send(Socket2, Posnew2),
-					
-					case lists:member(New_pos, Points) of
-						true -> Posnew3 = <<<<"3;">>/binary, New_pos/binary, <<"\n">>/binary>>,
-								NewPoint = lists:delete(New_pos, Points),
-								gen_tcp:send(Socket1, Posnew3),
-								gen_tcp:send(Socket2, Posnew3),
-								case lists:flatlength(NewPoint) of
-									0 ->
-										Posnew4 = <<"4; fini\n">>,
-										gen_tcp:send(Socket1, Posnew4),
-										gen_tcp:send(Socket2, Posnew4);
-									Nb -> io:format("Point +1 ~p ~n", [Nb])
-								end;
-						
-						false ->
-							NewPoint = Points,
-							io:format("No Point  ~n", [])
-					end,
-					State2 = State#state{player1=New_pos, points=NewPoint}
+					{NewPoint, Nb12, Nbpas2, Nbplay2, Nb22} = new_position(Pos, Socket1, Socket2, Nb1, Points, Nb2, <<"1">>, New_pos, Nbpas, Nbplay),					
+					State2 = State#state{player1=New_pos, points=NewPoint, nbsweet1=Nb12, nbsweet2=Nb22, nbpas=Nbpas2, nbplayer=Nbplay2}
 			end;
 		Socket2 ->   
-			[X11, Y11] = binary:split(P2, <<",">>),
-			X12 = list_to_integer(binary_to_list(X11)),
-			Y12 = list_to_integer(binary_to_list(Y11)),
-			X2 = mod(X12+X1, 20),
-			Y2 = mod(Y12+Y1, 20),
-%% 			New_pos = {element(1, P1)+X1, element(2, P1)+Y1},
-			S1 = list_to_binary(integer_to_list(X2)),
-			S2 = list_to_binary(integer_to_list(Y2)),
-%% 			New_pos = {element(1, P1)+X1, element(2, P1)+Y1},
-			New_pos = <<S1/binary, <<",">>/binary, S2/binary>>,
-					if
-						P1 == New_pos -> 
-							Reply = ko,
-									 State2 = State;
-						true -> 
-								Reply = ok,
-								State2 = State#state{player2=New_pos},
-								Posnew = <<<<"1;">>/binary, Pos/binary, <<"\n">>/binary>>,
-								Posnew2 = <<<<"2;">>/binary, Pos/binary, <<"\n">>/binary>>,
-%% 								io:format("New pos ~p ~n", [Posnew]),
-								gen_tcp:send(Socket2, Posnew),
-								gen_tcp:send(Socket1, Posnew2),
-								case lists:member(New_pos, Points) of
-										true -> Posnew3 = <<<<"3;">>/binary, New_pos/binary, <<"\n">>/binary>>,
-												NewPoint = lists:delete(New_pos, Points),
-												gen_tcp:send(Socket1, Posnew3),
-												gen_tcp:send(Socket2, Posnew3),
-									case lists:flatlength(NewPoint) of
-									0 ->
-										Posnew4 = <<"4; fini\n">>,
-										gen_tcp:send(Socket1, Posnew4),
-										gen_tcp:send(Socket2, Posnew4);
-									Nb -> io:format("Point +1 ~p ~n", [Nb])
-										end;
-										false ->
-											NewPoint = Points,
-												io:format("No Point ~n", [])
-							end,
-					State2 = State#state{player1=New_pos, points=NewPoint}
-					end
+			New_pos = calcul_newPos(P2, X1, Y1),
+			if
+				P1 == New_pos -> 
+					Reply = ko,
+					State2 = State;
+				true -> 
+					Reply = ok,
+					{NewPoint, Nb12, Nbpas2, Nbplay2, Nb22} = new_position(Pos, Socket2, Socket1, Nb2, Points, Nb1, <<"2">>, New_pos, Nbpas, Nbplay),					
+					State2 = State#state{player2=New_pos, points=NewPoint, nbsweet2=Nb12, nbsweet1=Nb22, nbpas=Nbpas2, nbplayer=Nbplay2}
+			end
 	end,
-    {reply, Reply, State2};
+	{reply, Reply, State2};
 handle_call({add_player, [S]}, _From, #state{nbplayer=Nbj}=State) ->
 	io:format("Add player ~p~n", [S]),
-    Reply = ok,
+	Reply = ok,
 	Nbj2 = Nbj+1,
-%% 	Points = mnesia:dirty_read(point),
-%% 	io:format("Point All ~p~n", [Points]),
+	%% 	Points = mnesia:dirty_read(point),
+	%% 	io:format("Point All ~p~n", [Points]),
 	State3 = State#state{nbplayer=Nbj2},
-    {reply, Reply, State3};
+	{reply, Reply, State3};
 handle_call({set_point, [Point]}, _From, #state{points=Points, nbpas=Pas}=State) ->	
-    Reply = ok,
+	Reply = ok,
 	Pas2 = Pas+1,
 	if
 		Pas2 == 1 ->
@@ -151,49 +89,51 @@ handle_call({set_point, [Point]}, _From, #state{points=Points, nbpas=Pas}=State)
 			State3 = State#state{points=Points2, nbpas=Pas2};
 		true -> State3 = State
 	end,
-    {reply, Reply, State3};
+	{reply, Reply, State3};
 handle_call({send_position, [S]}, _From, #state{nbplayer=Nbj, player1=_Pos_play1, socket1=S1, points=Points}=State) ->
-    Reply = ok,
+	Reply = ok,
+	Nbj2 = Nbj+1,
 	if
-		Nbj == 1 ->
-%% 			io:format("Nb joueur ~p~n", [Nbj]),
-%% 			Pos = {1, 1},
-			Pos2 = <<"0,0\n">>,
+		Nbj2 == 1 ->
+			%% 			io:format("Nb joueur ~p~n", [Nbj]),
+			%% 			Pos = {1, 1},
+			Pos2 = <<"5;0,0\n">>,
 			Pos21 = <<"0,0">>,
 			PlayC = #playercoord{nbplayer=1, coord= Pos21, nbsweet=0},
 			mnesia:dirty_write(PlayC),
 			send_pos(Pos2, S),
-			State2 = State#state{player1=Pos21, socket1=S},
+			State2 = State#state{nbplayer=Nbj2, player1=Pos21, socket1=S},
 			{reply, Reply, State2};
-		Nbj == 2 -> 
-%% 			io:format("Nb joueur ~p~n", [Nbj]),
-%% 			Pos1 = {19, 19},
-			Pos2 = <<"19,0\n">>,
+		Nbj2 == 2 -> 
+			%% 			io:format("Nb joueur ~p~n", [Nbj]),
+			%% 			Pos1 = {19, 19},
+			Pos2 = <<"5;19,0\n">>,
+			Pos22 = <<"6;19,0\n">>,
 			Pos21 = <<"19,0">>,
 			PlayC = #playercoord{nbplayer=2, coord= Pos21, nbsweet=0},
 			mnesia:dirty_write(PlayC),			
 			send_pos(Pos2, S),
-			send_pos(Pos2, S1),
-%% 			send_pos(<<"2">>, S),
-%% 			Pos_play12 = <<element(1, Pos_play1), >>,
-			Pos212 = <<"0,0\n">>,
+			send_pos(Pos22, S1),
+			%% 			send_pos(<<"2">>, S),
+			%% 			Pos_play12 = <<element(1, Pos_play1), >>,
+			Pos212 = <<"6;0,0\n">>,
 			send_pos(Pos212, S),
-%% 			Points2 = list_to_binary(Points),
-			Points3 = <<Points/binary, <<"\n">>/binary>>,
+			%% 			Points2 = list_to_binary(Points),
+			Points3 = << <<"7;">>/binary, Points/binary, <<"\n">>/binary>>,
 			io:format("Point All ~p~n", [Points3]),
 			send_pos(Points3, S1),
 			send_pos(Points3, S),
 			Pointbin = binary:split(Points, <<";">>, [global]),
-			State3 = State#state{player2=Pos21, socket2=S, points=Pointbin},
+			State3 = State#state{nbplayer=Nbj2, player2=Pos21, socket2=S, points=Pointbin},
 			{reply, Reply, State3};
 		true -> error_logger:info_msg("Full player ~p", []),
-			 {reply, Reply, State}
+				{reply, Reply, State}
 	end;
 handle_call(_Call, _From, State) -> {reply, ko, State}.
 
 -spec handle_info({nodedown, atom()}, state()) -> {stop, nodedown, state()} | {noreply, state()}.
 handle_info(_Info, State) ->
-  {noreply, State}.
+	{noreply, State}.
 
 %% @private
 -spec handle_cast(term(), state()) -> {noreply, state()}.
@@ -206,76 +146,75 @@ terminate(_Reason, _State) -> ok.
 code_change(_OldVersion, State, _Extra) -> {ok, State}.
 
 start_s() ->
-    case gen_tcp:listen(5678, [binary, {packet, 0}, 
-                                        {active, false}]) of
-        {ok, ListenSock} ->
-            start_servers(ListenSock);
-        {error,Reason} ->
-            {error,Reason}
-    end.
+	case gen_tcp:listen(5678, [binary, {packet, 0}, 
+							   {active, false}]) of
+		{ok, ListenSock} ->
+			start_servers(ListenSock);
+		{error,Reason} ->
+			{error,Reason}
+	end.
 
 start_servers(LS) ->
-    spawn(?MODULE,server,[LS]).
+	spawn(?MODULE,server,[LS]).
 
 server(LS) ->
-    case gen_tcp:accept(LS) of
-        {ok,S} ->
-			add_player(S),
-            spawn(?MODULE, loop, [S]),
+	case gen_tcp:accept(LS) of
+		{ok,S} ->
+%% 			add_player(S),
+			spawn(?MODULE, loop, [S]),
 			io:format("Socket ~p~n", [S]),
-%% 			send_position(S),
-            server(LS);
-        Other ->
-            io:format("accept returned ~w - goodbye!~n",[Other]),
-            ok
-    end.
+			%% 			send_position(S),
+			server(LS);
+		Other ->
+			io:format("accept returned ~w - goodbye!~n",[Other]),
+			ok
+	end.
 
 %%TODO : Test position et send ok or ko case down top left right
 loop(S) ->
-    case gen_tcp:recv(S, 0) of
-        {ok,Data} ->
-            io:format("Test ~p~n", [Data]),
-				case Data of					
-            		<<"ok">> -> 
-							  gen_tcp:send(S, <<"ok\n">>),
-							  send_position(S),
-							  put_coordonnees(10);
-					Other -> 	
-								test_position(S, Other)
-%% 								case Reply of
-%% 									ko -> gen_tcp:send(S, "ko\n");
-%% 									ok -> gen_tcp:send(S, "ok\n")
-%% 								end
-				end,
-            loop(S);
-        {error, closed} ->
-            io:format("Socket ~w closed [~w]~n",[S,self()]),
-            ok
-    end.
+	case gen_tcp:recv(S, 0) of
+		{ok,Data} ->
+			%%             io:format("Test ~p~n", [Data]),
+			case Data of					
+				<<"ok">> -> 
+					gen_tcp:send(S, <<"ok\n">>),
+					send_position(S),
+					put_coordonnees(10);
+				<<"newgame">> ->
+					restart(S);
+				Other -> 	
+					test_position(S, Other)
+			end,
+			loop(S);
+		{error, closed} ->
+			io:format("Socket ~w closed [~w]~n",[S,self()]),
+			ok
+	end.
 
 create_table() ->
 	io:format("Create table ~n", []),
-%% 	{ok, Pwd} = file:get_cwd(),
-%% 	mnesia:create_schema([node()]),
+	%% 	{ok, Pwd} = file:get_cwd(),
+	%% 	mnesia:create_schema([node()]),
 	mnesia:start(),
-%% 	{disc_copies, [node()]}, {disc_copies, [node()]},
+	%% 	{disc_copies, [node()]}, {disc_copies, [node()]},
 	Table = [
-					{attributes, record_info(fields, point)}],
+			 {attributes, record_info(fields, point)}],
 	Table1 = [
-					{attributes, record_info(fields, playercoord)}],
+			  {attributes, record_info(fields, playercoord)}],
 	Test = mnesia:create_table(point , Table),							  
 	io:format("Create table ~p~n", [Test]),
 	mnesia:create_table(playercoord, Table1).
 
+%%TODO check if already exist
 put_coordonnees(0) ->
 	ok;
 put_coordonnees(N) ->	
-	X = random:uniform(20),
-	Y = random:uniform(20),
+	X = random:uniform(20) -1,
+	Y = random:uniform(20) -1,
 	S1 = list_to_binary(integer_to_list(X)),
 	S2 = list_to_binary(integer_to_list(Y)),
-%% 	Pos = <<S1/binary, <<",">>/binary , S2/binary>>,
-%% 	io:format("Put Position ~p~n", [Pos]),
+	%% 	Pos = <<S1/binary, <<",">>/binary , S2/binary>>,
+	%% 	io:format("Put Position ~p~n", [Pos]),
 	Point = #point{coordX=S1, coordY=S2},
 	mnesia:dirty_write(Point),
 	Point2 = <<S1/binary,<<",">>/binary, S2/binary>>,
@@ -289,3 +228,72 @@ send_pos(Pos, S) ->
 mod(X,Y) when X > 0 -> X rem Y;
 mod(X,Y) when X < 0 -> Y + X rem Y;
 mod(0,_Y) -> 0.
+
+new_position(Pos, Socket1, Socket2, Nb1, Points, Nb2, Player, New_pos, Nbpas, Nbplay) ->
+	Posnew = <<<<"1;">>/binary, Pos/binary, <<"\n">>/binary>>,
+	Posnew2 = <<<<"2;">>/binary, Pos/binary, <<"\n">>/binary>>,								
+	gen_tcp:send(Socket1, Posnew),
+	gen_tcp:send(Socket2, Posnew2),	
+	case lists:member(New_pos, Points) of
+		true -> {Nb12, NewPoint, Nbpas2, Nbplay2, Nb22} = test_point(Socket1, Socket2, New_pos, Points, Nb1, Nb2, Player, Nbpas, Nbplay);
+		false ->
+			Nbplay2 = Nbplay,
+			Nbpas2 = Nbpas,
+			Nb12 = Nb1,
+			Nb22 = Nb2,
+			NewPoint = Points
+	end,
+	{NewPoint, Nb12, Nbpas2, Nbplay2, Nb22}.
+
+test_point(Socket1, Socket2, New_pos, Points, Nb1, Nb2, Player, Nbpas, Nbplay) ->
+	Nb12 = Nb1 + 1,
+	Nb13 = list_to_binary(integer_to_list(Nb12)),
+	Posnew3 = <<<<"3;">>/binary, New_pos/binary, <<";">>/binary, Nb13/binary, <<";">>/binary, Player/binary, <<"\n">>/binary>>,
+	NewPoint = lists:delete(New_pos, Points),
+	gen_tcp:send(Socket1, Posnew3),
+	gen_tcp:send(Socket2, Posnew3),
+	case lists:flatlength(NewPoint) of
+		0 ->
+			Nbpas2 = 0,
+			Nbplay2 = 0,
+			if
+				Nb12 > Nb2 ->	Posnew4 = << <<"4;">>/binary, Player/binary, <<"\n">>/binary >>,
+								Posnew5 = <<"4;other\n">>,
+								gen_tcp:send(Socket1, Posnew4),
+								gen_tcp:send(Socket2, Posnew5);
+				Nb12 == Nb2 -> 	Posnew4 = <<"4;equals\n">>,
+								gen_tcp:send(Socket1, Posnew4),
+								gen_tcp:send(Socket2, Posnew4);
+				true -> 		Posnew4 = <<"4;other\n">>,
+								Posnew5 = << <<"4;">>/binary, Player/binary, <<"\n">>/binary >>,
+								gen_tcp:send(Socket1, Posnew4),
+								gen_tcp:send(Socket2, Posnew5)
+			end,
+			Nb122 = 0,
+			Nb22 = 0;
+		Nb -> 
+			Nb122 = Nb12,
+			Nb22 = Nb2,
+			Nbplay2 = Nbplay,
+			Nbpas2 = Nbpas,
+			io:format("Point +1 ~p ~n", [Nb])
+	end,
+	{Nb122, NewPoint, Nbpas2, Nbplay2, Nb22}.
+
+calcul_newPos(P1, X1, Y1) ->
+	[X11, Y11] = binary:split(P1, <<",">>),
+	X12 = list_to_integer(binary_to_list(X11)),
+	Y12 = list_to_integer(binary_to_list(Y11)),
+	X2 = mod(X12+X1, 20),
+	Y2 = mod(Y12+Y1, 20),
+	S1 = list_to_binary(integer_to_list(X2)),
+	S2 = list_to_binary(integer_to_list(Y2)),
+	<<S1/binary, <<",">>/binary, S2/binary>>.
+
+%%TODO erase table, new point , replace player
+restart(Socket) ->	
+	put_coordonnees(10),
+%% 	add_player(Socket),
+	gen_tcp:send(Socket, <<"ok\n">>),
+	send_position(Socket).
+	
